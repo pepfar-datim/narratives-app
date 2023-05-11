b7c98f164200000f9f5a01fcf1c29ae70fe00631
jason-p-pickering <jason.p.pickering@gmail.com>
2023-05-11 06:57:04 +0200

    Add bearer token to requests

--- utils.R
+++ utils.R
@@ -31,7 +31,10 @@ isUSGUser<-function(d2_session) {
   
     paste0(d2_session$base_url, "api/me?fields=userGroups[id,name]") %>% 
     utils::URLencode(.) %>% 
-    httr::GET(.,handle=d2_session$handle) %>% 
+    httr::GET(.,handle=d2_session$handle,
+              httr::add_headers(Authorization =
+                                  paste("Bearer",
+                                        d2_session$token$credentials$access_token, sep = " "))) %>% 
     httr::content(.,"text") %>% 
     jsonlite::fromJSON() %>% 
     purrr::pluck("userGroups") %>% 
@@ -45,7 +48,10 @@ getOperatingUnits<-function(d2_session) {
   
   ou_map<-paste0(d2_session$base_url,"api/dataStore/dataSetAssignments/orgUnitLevels") %>% 
     URLencode(.) %>% 
-    httr::GET(., handle=d2_session$handle) %>% 
+    httr::GET(., handle=d2_session$handle,
+              httr::add_headers(Authorization =
+                                  paste("Bearer",
+                                        d2_session$token$credentials$access_token, sep = " "))) %>% 
     httr::content(.,"text") %>% 
     jsonlite::fromJSON(.) %>% 
     dplyr::bind_rows() %>% 
@@ -58,7 +64,10 @@ getOperatingUnits<-function(d2_session) {
   #TODO: Investigate why the API is not filtering based on the users orgunit, which really should be the case.
   ous<-paste0(d2_session$base_url,"api/organisationUnits?filter=level:lt:5&fields=id,name&filter=path:like:",d2_session$user_orgunit,"&paging=false") %>% 
     URLencode(.) %>% 
-    httr::GET(., handle=d2_session$handle) %>% 
+    httr::GET(., handle=d2_session$handle,
+              httr::add_headers(Authorization =
+                                  paste("Bearer",
+                                        d2_session$token$credentials$access_token, sep = " "))) %>% 
     httr::content(.,"text") %>% 
     jsonlite::fromJSON(.) %>% 
     purrr::pluck("organisationUnits")
@@ -86,7 +95,10 @@ getUSGNarrativeDataElements<-function(d2_session) {
   
   paste0(d2_session$base_url,"api/dataSets/wkdCW3M4zYT?fields=id,dataSetElements,organisationUnits") %>% 
     URLencode(.) %>% 
-    httr::GET(., handle=d2_session$handle) %>% 
+    httr::GET(., handle=d2_session$handle,
+              httr::add_headers(Authorization =
+                                  paste("Bearer",
+                                        d2_session$token$credentials$access_token, sep = " "))) %>% 
     httr::content(.,"text") %>% 
     jsonlite::fromJSON(.) %>% 
     purrr::pluck("dataSetElements") %>% 
@@ -98,7 +110,10 @@ getNarrativeDataElements<-function(fiscal_year, type="Results", d2_session) {
   
   
   des<- paste0(d2_session$base_url,"api/dataElementGroups?filter=name:like:Narratives&paging=false&fields=id,name,dataElements[id,shortName]") %>% 
-    httr::GET(.,handle=d2_session$handle) %>% 
+    httr::GET(.,handle=d2_session$handle,
+              httr::add_headers(Authorization =
+                                  paste("Bearer",
+                                        d2_session$token$credentials$access_token, sep = " "))) %>% 
     httr::content("text") %>% 
     jsonlite::fromJSON() %>% 
     purrr::pluck("dataElementGroups") %>%
@@ -112,15 +127,23 @@ getNarrativeDataElements<-function(fiscal_year, type="Results", d2_session) {
   
   #Technical area
   
-  tech_area<-paste0(d2_session$base_url,"api/dataElementGroupSets/LxhLO68FcXm?fields=dataElementGroups[id,name,dataElements[id]") %>% 
-  httr::GET(.,handle=d2_session$handle) %>% httr::content("text") %>% jsonlite::fromJSON() %>% 
+  tech_area <-
+    paste0(
+      d2_session$base_url,
+      "api/dataElementGroupSets/LxhLO68FcXm?fields=dataElementGroups[id,name,dataElements[id]"
+    ) %>%
+    httr::GET(., handle = d2_session$handle,       httr::add_headers(
+      Authorization =
+        paste("Bearer",
+              d2_session$token$credentials$access_token, sep = " ")
+    )) %>% httr::content("text") %>% jsonlite::fromJSON() %>%
     purrr::pluck("dataElementGroups") %>%
-    tidyr::unnest_longer(.,"dataElements",simplify = TRUE) %>% 
-    dplyr::mutate( de_uid = dataElements$id) %>% 
-    dplyr::select(-id,-dataElements) %>% 
-    dplyr::rename("technical_area" = name )
+    tidyr::unnest_longer(., "dataElements", simplify = TRUE) %>%
+    dplyr::mutate(de_uid = dataElements$id) %>%
+    dplyr::select(-id, -dataElements) %>%
+    dplyr::rename("technical_area" = name)
   
-  des<- des %>% dplyr::inner_join(tech_area,by="de_uid") 
+  des <- des %>% dplyr::inner_join(tech_area, by = "de_uid") 
   
   getListElement<-function(x,n) tryCatch(str_trim(str_split(x,","))[[n]],  error = function(e) return(NA))
   #Tech area
@@ -224,7 +247,10 @@ getUserMechanisms<-function(d2_session) {
   
   mechs<-paste0(d2_session$base_url,"api/categoryOptionCombos?filter=categoryCombo.id:eq:wUpfppgjEza&fields=id,code,name,categoryOptions[id,organisationUnits[id,name]&paging=false") %>% 
     URLencode(.) %>%
-    httpcache::GET(., handle=d2_session$handle) %>% 
+    httpcache::GET(., handle=d2_session$handle, 
+                   httr::add_headers(Authorization =
+                                       paste("Bearer",
+                                             d2_session$token$credentials$access_token, sep = " "))) %>% 
     httr::content(.,"text") %>% 
     jsonlite::fromJSON(.,simplifyDataFrame = TRUE) %>% 
     purrr::pluck("categoryOptionCombos") %>% 
@@ -245,7 +271,10 @@ getUserMechanisms<-function(d2_session) {
   
 
   cogs<-paste0(d2_session$base_url,"api/",api_version(),"/dimensions/SH885jaRe0o/items.json?fields=id,shortName&paging=false") %>% 
-    httr::GET(., handle = d2_session$handle) %>% 
+    httr::GET(., handle = d2_session$handle,
+              httr::add_headers(Authorization =
+                                  paste("Bearer",
+                                        d2_session$token$credentials$access_token, sep = " "))) %>% 
     httr::content(.,"text") %>% 
     jsonlite::fromJSON(.) %>% 
     purrr::pluck("items")
@@ -256,7 +285,10 @@ getUserMechanisms<-function(d2_session) {
   #Agency map
   agencies_cos<-paste0(d2_session$base_url,"api/categoryOptionGroupSets/bw8KHXzxd9i?fields=categoryOptionGroups[id,name,categoryOptions[id]]&paging=false") %>% 
    URLencode(.) %>% 
-    httr::GET(., handle = d2_session$handle) %>% 
+    httr::GET(., handle = d2_session$handle,
+              httr::add_headers(Authorization =
+                                  paste("Bearer",
+                                        d2_session$token$credentials$access_token, sep = " "))) %>% 
     httr::content(.,"text") %>% 
     jsonlite::fromJSON(.) %>% 
     purrr::pluck("categoryOptionGroups") %>% 
@@ -270,7 +302,10 @@ getUserMechanisms<-function(d2_session) {
   #Partner map
   partners_cos<-paste0(d2_session$base_url,"api/categoryOptionGroupSets/BOyWrF33hiR?fields=categoryOptionGroups[id,name,categoryOptions[id]]&paging=false") %>% 
     URLencode(.) %>% 
-    httr::GET(., handle = d2_session$handle) %>% 
+    httr::GET(., handle = d2_session$handle, 
+              httr::add_headers(Authorization =
+                                  paste("Bearer",
+                                        d2_session$token$credentials$access_token, sep = " "))) %>% 
     httr::content(.,"text") %>% 
     jsonlite::fromJSON(.) %>% 
     purrr::pluck("categoryOptionGroups") %>% 
@@ -310,7 +345,10 @@ d2_analyticsResponse <- function(url,remapCols=TRUE, d2_session) {
 
   #print(url)
   d<-url %>% 
-    httr::GET(., handle = d2_session$handle) %>% 
+    httr::GET(., handle = d2_session$handle,
+              httr::add_headers(Authorization =
+                                  paste("Bearer",
+                                        d2_session$token$credentials$access_token, sep = " "))) %>% 
     httr::content(.,"text") %>% 
     jsonlite::fromJSON(.)
   
